7811ac6e5a2d8ebdac8243fc096f1da55671b497
sanjaydambal <sanjaydambal@gmail.com>
2024-03-14 12:06:32 +0530

    created the app component and provided with a userdata state and handlesearch function

---
+++ src/ProfileDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileDisplay = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfileDisplay
---
+++ src/ProfileSearch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileSearch = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfileSearch
