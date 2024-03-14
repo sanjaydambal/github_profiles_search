e5389942e1cbd35f51842393246812235a6ec34c
sanjaydambal <sanjaydambal@gmail.com>
2024-03-14 13:01:49 +0530

    wrapped the response in try and error in ctach component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import ProfileDisplay from './ProfileDisplay';
 function App() {
   const [userData, setUserData] = React.useState(null)
 const handleSearch = async (user) => {
-  const res = await fetch(`https://api.github.com/users/${user}`)
-  const data = await res.json()
-  setUserData(data)
+  try {
+    const res = await fetch(`https://api.github.com/users/${user}`)
+    const data = await res.json()
+    setUserData(data)
+  } catch (error) {
+    console.log(error)
+  }
+ 
 }
   return (
     <div className="App">
- <h1 className="d-flex justify-content-center"> GitHub Profile Search App</h1>
+ <h1 > GitHub Profile Search App</h1>
  <ProfileSearch handleSearch={handleSearch} />
  {userData && <ProfileDisplay data={userData} /> }
     </div>
