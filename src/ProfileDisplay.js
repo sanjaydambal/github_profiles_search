c740c451262608c6260d7f49666639ef1e577c54
sanjaydambal <sanjaydambal@gmail.com>
2024-03-14 12:10:46 +0530

    created profiledisplay which recieves data as prop from app component and diaplays user profile

--- src/ProfileDisplay.js
+++ src/ProfileDisplay.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const ProfileDisplay = () => {
+const ProfileDisplay = ({ data }) => {
   return (
-    <div>
-      
-    </div>
+    <div className="d-flex justify-content-center flex-column align-items-center">
+    <img src={data.avatar_url} alt="user avatar" style={{ width: '200px', height: '200px' }} />
+    <h3 className="mt-3">{data.name}</h3>
+    <a href={data.html_url}>View Profile</a>
+  </div>
   )
 }
 
